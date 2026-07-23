accd723541b128c696ae7f5d25a6a6c2308a4d2c
SebitasDown <areizasebas31@gmail.com>
2026-07-22 22:18:25 -0500

    fix: update database connection string handling in PrismaService

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -8,8 +8,10 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   private readonly logger = new Logger(PrismaService.name);
 
   constructor() {
+    const dbUrl = new URL(process.env.DATABASE_URL!);
+    dbUrl.searchParams.delete('sslmode');
     const pool = new Pool({
-      connectionString: process.env.DATABASE_URL,
+      connectionString: dbUrl.toString(),
       ssl: { rejectUnauthorized: false },
     });
     const adapter = new PrismaPg(pool);
